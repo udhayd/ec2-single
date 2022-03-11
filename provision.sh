18f19126dcd4db4d935ac131b98eab5464c15dd7
name <you@example.com>
2022-03-11 10:13:32 +0000

    Update

--- provision.sh
+++ provision.sh
@@ -21,9 +21,9 @@ then
     exit
 fi
 
-env|egrep -w 'AWS_CONTAINER_AUTHORIZATION_TOKEN|AWS_DEFAULT_REGION' >/dev/null 2>&1 || env|egrep -w 'AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY|AWS_DEFAULT_REGION' >/dev/null 2>&1
-
-if [ $? -ne 0 ]
+env|egrep -w 'AWS_CONTAINER_AUTHORIZATION_TOKEN' >/dev/null 2>&1 || env|egrep -w 'AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY|AWS_DEFAULT_REGION' >/tmp/aws 2>&1
+cnt=$(wc -l /tmp/aws|awk '{print $1}')
+if [ $cnt -ne 3 ]
 then
     echo -e '\n' "One of following variables missing 'AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY|AWS_DEFAULT_REGION', Please configure the same" '\n'
     exit
